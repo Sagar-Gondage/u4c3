09e73fd3f93c5ef9500567ab68f6cbd5c622ddbc
Sagar-Gondage <sagargondage370@gmail.com>
2022-06-09 10:14:06 +0530

    css added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Route, Routes } from "react-router-dom";
+
+import RequiredAuth from "./hoc/RequiredAuth";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Navbar from "./components/Navbar";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Navbar />
+            <Routes>
+                <Route path="login" element={<Login />} />
+                <Route path="" element={
+                    <RequiredAuth>
+                        <Home />
+                    </RequiredAuth>
+                }
+                />
+            </Routes>
+        </div>
+    );
 }
 
 export default App;
